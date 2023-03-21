0cd9b4c5daf9e8fc3719b59eaff4c330a1a4375b
Baoshuo <i@baoshuo.ren>
2023-03-21 20:13:51 +0800

    feat: add dockerfile

--- db/luogu_remote_judge.sql
+++ db/luogu_remote_judge.sql
@@ -1,9 +1,3 @@
-ALTER TABLE
-    `problems`
-ADD
-    `type` varchar(20) NOT NULL DEFAULT 'local';
-
-ALTER TABLE
-    `problems`
-ADD
-    KEY `type` (`type`);
+ALTER TABLE `problems` ADD `type` varchar(20) NOT NULL DEFAULT 'local';
+ALTER TABLE `problems` ADD KEY `type` (`type`);
+insert into judger_info (judger_name, password) values ('luogu_remote_judger', '_judger_password_');
